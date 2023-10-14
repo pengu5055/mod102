591ddc193adfe818a1fc82e3184e80405678d67e
Marko Urbanc <urbancmarko1@gmail.com>
2023-10-14 17:14:49 +0200

    feat: :sparkles: define basic model in PuLP terms

--- Code/basic.py
+++ Code/basic.py
@@ -10,4 +10,35 @@ import cmasher as cmr
 
 df = pd.read_table('table.dat', sep=',', skiprows=2, index_col=0)
 df.drop(df.columns[[-1, -2]], axis=1, inplace=True)
-print(df)
+
+# Create the 'prob' variable to contain the problem data
+prob = LpProblem("Diet Problem", LpMinimize)
+
+# Create a list of the food items
+food_items = list(df.index)
+
+# Create variables. These variables are the amounts of each food item to buy
+food_vars = LpVariable.dicts("Food", food_items, lowBound=0, cat='Continuous')
+
+# Define objective function and add it to the problem
+prob += lpSum([df.loc[i, 'Energija[kcal]'] * food_vars[i] for i in food_items]), "Total energy intake per person"
+
+# And now we can add the constraints
+prob += lpSum([df.loc[i, 'Mascobe[g]'] * food_vars[i] for i in food_items]) >= 70, "FatRequirement"
+prob += lpSum([df.loc[i, 'Ogljikovi_Hidrati[g]'] * food_vars[i] for i in food_items]) >= 310, "CarbohydrateRequirement"
+prob += lpSum([df.loc[i, 'Beljakovine[g]'] * food_vars[i] for i in food_items]) >= 50, "ProteinRequirement"
+prob += lpSum([df.loc[i, 'Ca[mg]'] * food_vars[i] for i in food_items]) >= 1000, "CalciumRequirement"
+prob += lpSum([df.loc[i, 'Fe[mg]'] * food_vars[i] for i in food_items]) >= 18, "IronRequirement"
+
+prob.writeLP("DietProblem_Model.lp")
+
+# Slove the problem
+prob.solve()
+
+# Print the status of the solution
+print("Status:", LpStatus[prob.status])
+
+# Print the optimal solution
+for v in prob.variables():
+    print(v.name, "=", v.varValue)
+print("Total energy intake per person = ", value(prob.objective))
---
+++ README.md
@@ -0,0 +1,19 @@
+# **Linear programing**
+
+## **Basic problem**
+Let us minimize the amount of calories, thus creating a diet plan if we need to meet the following
+conditions:  
+
+* 70 g of fats
+* 310 g of carbohydrates
+* 50 g of proteins
+* 1000 mg of calcium
+* 18 mg of of iron
+
+Additionally it must hold:
+
+* Daily intake of food is < 2 kg
+* 60 mg of vitamin C
+* 3500 mg of potassium
+* acceptable level of sodium (500 - 2400 mg)
+
