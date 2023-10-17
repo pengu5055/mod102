e10d66923d28cf2b7f42ab5ca6b281c036ac5d6c
Marko Urbanc <urbancmarko1@gmail.com>
2023-10-18 00:58:06 +0200

    feat: :sparkles: last step of preprocessing of expanded dataset

---
+++ Code/test-data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+# Read in the data
+df = pd.read_hdf("Data/FoodData.h5")
+
+# Reduce each list of brand_owner to a single string 
+df['brand_owner'] = df['brand_owner'].apply(lambda x: x[0])
+
+df.to_hdf("Data/FoodData_processed.h5", index=True, complevel=9, 
+                              key="FoodData_processed", mode="w")
