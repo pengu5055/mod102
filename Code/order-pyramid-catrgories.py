2cbce3debb6a804c17610ff6ff7da58028521e44
Marko Urbanc <urbancmarko1@gmail.com>
2023-10-17 22:00:00 +0200

    perf: :zap: change map format for easier reading

--- Code/order-pyramid-catrgories.py
+++ Code/order-pyramid-catrgories.py
@@ -57,8 +57,9 @@ Strange (backup category for items that do not fit into any of the above categor
                   
 Map the 429 categories to the 12 categories. You are NOT allowed to make up any more categories
 than the ones I've given you. Make sure that each category is mapped to exactly one of the 12 
-categories. The mapping should be a Python dictionary with the 429 categories as keys and the 12 
-categories as values. For example anything related to Desserts should be mapped to the category
+categories. The mapping should be a comma delimited file with the 429 categories as rows and the 12 
+categories as values for the second column where the category is specified. The first one is for the original
+category name. For example anything related to Desserts should be mapped to the category
 "Sugar and sugar products". 
 
 Here is the list of 429 categories:
